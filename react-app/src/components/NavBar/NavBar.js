e4dac2d5ad82434cd807fabfb02333035c5af907
Josh Salcido <joshuasalcido21@gmail.com>
2022-08-04 00:16:12 -0600

    added some minor css and created post button

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,35 +1,32 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import LogoutButton from '../auth/LogoutButton';
 
 const NavBar = () => {
   return (
     <nav>
       <ul>
-        <li>
           <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+            meScroll
           </NavLink>
-        </li>
-        <li>
+        <button className='create-post-btn'>Create Post</button>
+        {/* <li>
           <NavLink to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
-        </li>
-        <li>
+        </li> */}
+        {/* <li>
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
-        </li>
+        </li> */}
         {/* <li>
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
         </li> */}
-        <li>
           <LogoutButton />
-        </li>
       </ul>
     </nav>
   );
